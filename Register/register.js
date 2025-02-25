c8f57286c52949cbea14b7928396dde3c1ac2ea3
Francesco <martinifranky98@gmail.com>
2025-02-25 16:32:45 +0100

    modificato form registrazione

--- Register/register.js
+++ Register/register.js
@@ -1,47 +1,58 @@
-function showExtraField() {
+function showExtraField()
+{
     var userType = document.getElementById('user-type').value;
     var coachField = document.getElementById('coach-field');
 
-    if (userType === 'Coach') {
+    if (userType === 'Coach')
+    {
         coachField.style.display = 'block';
-    } else {
+    } else
+    {
         coachField.style.display = 'none';
     }
 }
 
-    // Funzione per aggiungere un nuovo utente (endpoint riservato all'admin)
-    function addUser(newUser) {
-        fetch('http://localhost:8080/users/addUser', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newUser)
-        })
-        .then(response => {
-            if (!response.ok) {
+// Funzione per aggiungere un nuovo utente (endpoint riservato all'admin)
+function addUser(newUser)
+{
+    fetch('http://localhost:8080/users/addUser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser)
+    })
+        .then(response =>
+        {
+            if (!response.ok)
+            {
                 throw new Error('Non autorizzato o errore durante l\'aggiunta dell\'utente');
             }
             return response.json();
         })
-        .then(data => {
+        .then(data =>
+        {
             console.log('Risposta addUser:', data);
             //window.location.href="../Login/login.html";
             //printOutput(data);
         })
-        .catch(error => {
+        .catch(error =>
+        {
             console.error('Errore nell\'aggiunta dell\'utente:', error);
             //printOutput({ error: error.message });
         });
-      }
+}
 
-document.getElementById('registration-form').addEventListener('submit', function(event) {
+document.getElementById('registration-form').addEventListener('submit', function (event)
+{
     event.preventDefault();
     let roleNumber;
 
-    if(document.getElementById("user-type").value == "User"){
+    if (document.getElementById("user-type").value == "User")
+    {
         roleNumber = 0;
         const newUser = {
+            gym: document.getElementById('gym-selector').value,
             firstName: document.getElementById('name').value,
             lastName: document.getElementById('surname').value,
             email: document.getElementById('email').value,
@@ -49,11 +60,13 @@ document.getElementById('registration-form').addEventListener('submit', function
             role: roleNumber,
             secretCode: 0
         };
-    console.log(newUser.role, newUser.name, newUser.surname, newUser.email, newUser.password);  
-    addUser(newUser);
-    } else{
+        console.log(newUser.gym,newUser.role, newUser.name, newUser.surname, newUser.email, newUser.password);
+        addUser(newUser);
+    } else
+    {
         roleNumber = 1;
         const newUser = {
+            gym: document.getElementById('gym-selector').value,
             firstName: document.getElementById('name').value,
             lastName: document.getElementById('surname').value,
             email: document.getElementById('email').value,
@@ -61,7 +74,28 @@ document.getElementById('registration-form').addEventListener('submit', function
             role: roleNumber,
             secretCode: document.getElementById('code-coach').value
         };
-    console.log(newUser.role, newUser.name, newUser.surname, newUser.email, newUser.password);
-    addUser(newUser);
+        console.log(newUser.gym, newUser.role, newUser.name, newUser.surname, newUser.email, newUser.password);
+        addUser(newUser);
     }
 });
+
+function fetchGyms()
+{
+    fetch('http://localhost:8080/gyms')
+        .then(response => response.json())
+        .then(data =>
+        {
+            const gymSelector = document.getElementById('gym-selector');
+            gymSelector.innerHTML = '<option selected disabled>Seleziona una palestra...</option>';
+            data.forEach(gym => 
+            {
+                const option = document.createElement('option');
+                option.value = gym.id;
+                option.style.color = 'black';
+                option.textContent = 'Palestra ' + gym.id;
+                gymSelector.appendChild(option);
+            });
+        }).catch(error => { console.error('Errore nel recupero palestre', error); });
+}
+
+document.addEventListener('DOMContentLoaded', fetchGyms);
