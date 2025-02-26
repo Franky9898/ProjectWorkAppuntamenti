f2f9d131bdf798f7aa3a6818633e2598f3145357
WiliamDeMicco <summonerchaos1997@hotmail.com>
2025-02-26 12:36:00 +0100

    bottone switch login to profile  e altre modifiche per stili di pagine

--- Programma/dettagli-attivita.js
+++ Programma/dettagli-attivita.js
@@ -40,3 +40,14 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'selezione-attivita.html'; // Reindirizza alla pagina di selezione
     }
 });
+
+//Funzione di switch del  bottone login/register al bottone Profilo
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.getElementById('loginButton');
+    const token = localStorage.getItem('authToken');
+
+    if(token != null) {
+        loginButton.textContent = 'Profilo';
+        loginButton.href = '/profile.html';
+    }    
+});
